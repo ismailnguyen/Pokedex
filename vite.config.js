46724cf9cbe137858d71a95d32eb658e4d0e2e12
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-06-24 14:00:11 +0800

    fix manifest

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,49 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pokedex',
+        short_name: 'Pokedex',
+        description: 'Pokemon list and details',
+        start_url: '/',
+        scope: '/',
+        dir: 'ltr',
+        lang: 'en',
+        display: 'standalone',
+        orientation: 'any',
+        theme_color: '#D02434',
+        background_color: '#D02434',
+        related_applications: [],
+        prefer_related_applications: false,
+        icons: [{
+            src: 'images/icons/icon_16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          }, {
+            src: 'images/icons/icon_32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          }, {
+            src: 'images/icons/icon_64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          }, {
+            src: 'images/icons/icon_128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          }, {
+            src: 'images/icons/icon_256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          }, {
+            src: 'images/icons/icon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }]
+      }
+    })
   ],
   resolve: {
     alias: {
