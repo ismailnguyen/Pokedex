37d55309822897b5e4c62af44a184bd5dff35ea4
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-07-02 19:28:20 +0800

    fix favicon urls

--- vite.config.js
+++ vite.config.js
@@ -25,27 +25,27 @@ export default defineConfig({
         related_applications: [],
         prefer_related_applications: false,
         icons: [{
-            src: 'images/icons/icon_16.png',
+            src: 'favicon_16.png',
             sizes: '16x16',
             type: 'image/png'
           }, {
-            src: 'images/icons/icon_32.png',
+            src: 'favicon_32.png',
             sizes: '32x32',
             type: 'image/png'
           }, {
-            src: 'images/icons/icon_64.png',
+            src: 'favicon_64.png',
             sizes: '64x64',
             type: 'image/png'
           }, {
-            src: 'images/icons/icon_128.png',
+            src: 'favicon_128.png',
             sizes: '128x128',
             type: 'image/png'
           }, {
-            src: 'images/icons/icon_256.png',
+            src: 'favicon_256.png',
             sizes: '256x256',
             type: 'image/png'
           }, {
-            src: 'images/icons/icon_512.png',
+            src: 'favicon_512.png',
             sizes: '512x512',
             type: 'image/png'
           }]
